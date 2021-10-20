0e35903d09c3b1bbf18c1d7c410c2ef89bafa5e1
dari <hernandezdarifrancisco@gmail.com>
2021-10-20 13:57:51 -0500

    Hotfix responsive menu
    Show and hide menu for mobile sizes

--- code/index.js
+++ code/index.js
@@ -1,51 +1,21 @@
-var selector_nav = "header > nav"
-
-function hide_show (selector="", toggle=false, hide=false, show=false) {
-    // Add or remove hide class from specific element 
-    
-    elem = document.querySelector (selector)
-
-
-    if (toggle) {
-        elem.classList.toggle("hide")
-    }
-
-    if (hide) {
-        elem.classList.add("hide")
-    }
-
-    if (show) {
-        elem.classList.remove("hide")
-    }
-}
-
-function responsive () {
-    // Update page option when resize
-
-    // Hide / Show burger menu with media query
-    if (window.matchMedia("(max-width: 950px)").matches) {
-        hide_show (selector_nav, false, true, false)
-        
-    }
-    
-    if (window.matchMedia("(min-width: 951px)").matches) {
-        hide_show (selector_nav, false, false, true)
-    }
-}
-
-// Update responsive when page load
-responsive ()
-
-// Detect windows resize
-window.addEventListener('resize', responsive, false)
-
-// Hide / Show burger menu when press button
+var header = document.querySelector("header")
+var nav_elem = document.querySelector("header > nav")
 var btn_menu = document.querySelector (".icon-menu")
-btn_menu.addEventListener ("click", function() {
-    hide_show (selector_nav, toggle=true)
-}, false )
 
+header.addEventListener ("click", function(e) {
 
+    // Show menu
+    click_target = e.target.tagName
+    button_targets = ["DIV", "SPAN"]    
+    if (button_targets.includes(click_target)) {
+        nav_elem.classList.remove ("hide")        
+    } else {
+        nav_elem.classList.add ("hide")  
+    }
+})
 
+if (window.matchMedia("(max-width: 950px)").matches) {
+    nav_elem.classList.add ("hide")
+} 
 
 
