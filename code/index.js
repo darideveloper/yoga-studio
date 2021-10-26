1078364aef3ee19ca12d453897d7c05924330b1d
dari <hernandezdarifrancisco@gmail.com>
2021-10-26 00:11:36 -0500

    hotfix: hide menu in desktop size
    Onlyt hide menu / header in small sizes

--- code/index.js
+++ code/index.js
@@ -2,20 +2,29 @@ var header = document.querySelector("header")
 var nav_elem = document.querySelector("header > nav")
 var btn_menu = document.querySelector (".icon-menu")
 
-header.addEventListener ("click", function(e) {
+const header_query = window.matchMedia ('screen and (max-width: 950px)')
 
-    // Show menu
-    click_target = e.target.tagName
-    button_targets = ["DIV", "SPAN"]    
-    if (button_targets.includes(click_target)) {
-        nav_elem.classList.remove ("hide")        
-    } else {
-        nav_elem.classList.add ("hide")  
-    }
-})
+if (window.matchMedia ("(max-width: 950px)").matches) {
+    // Menu event listeners onkly for small screens
+
+    header.addEventListener ("click", function(e) {
+    
+        // Show menu
+        click_target = e.target.tagName
+        button_targets = ["DIV", "SPAN"]    
+        if (button_targets.includes(click_target)) {
+            nav_elem.classList.remove ("hide")        
+        } else {
+            nav_elem.classList.add ("hide")  
+        }
+    })
+
+}
+
+
 
 if (window.matchMedia("(max-width: 950px)").matches) {
+    // Hide original menu items
     nav_elem.classList.add ("hide")
 } 
-
 
